af9ac057b09a7a0e83981feedacb477202747a0f
akoobeeEy <119799055+akoobeeEy@users.noreply.github.com>
2024-09-25 19:14:25 +0500

    adding context

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import "react-toastify/dist/ReactToastify.css";
-import { UserProvider } from "./context/UserContext.jsx";
+import { Context } from "./context/UserContext.jsx";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <UserProvider>
+    <Context>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
-    </UserProvider>
+    </Context>
   </React.StrictMode>
 );
